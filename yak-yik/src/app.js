07af2c0c18f5b824c5295fc2b612bd55d2ade463
gucci-ninja <beautyspell5@gmail.com>
2018-06-07 02:39:07 -0400

    first react component

---
+++ yak-yik/src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react' // Component is a submodule
+// var React = require('react') --- this is es5, we are using es6
+
+class App extends Components {
+    render(){
+        return (
+            <div>Hello React!
+                </div>
+        )
+    }
+}
