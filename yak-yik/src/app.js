3e6d1f462f82fc2239163f17dd363f26f420d84c
gucci-ninja <beautyspell5@gmail.com>
2018-06-08 01:27:30 -0400

    react code

--- yak-yik/src/app.js
+++ yak-yik/src/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react' // Component is a submodule
 // var React = require('react') --- this is es5, we are using es6
+import ReactDOM from 'react-dom'
+import Zones from './components/Zones'
 
-class App extends Components {
+// App is basically an html tag that we are gonna render in the dom
+class App extends Component {
     render(){
         return (
-            <div>Hello React!
-                </div>
+            <div>
+                Hello React! HOw are you doing? yoyo
+                <Zones />
+            </div>
         )
     }
 }
+
+// use react-dome library to render code
+ReactDOM.render(<App />, document.getElementById('root')) // it'll take over the root tage with App
---
+++ yak-yik/src/components/Zones.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react' // Component is a submodule
+
+class Zones extends Component {
+    render(){
+        return (
+            <div>
+                Zones!
+            </div>
+        )
+    }
+}
+
+export default Zones
