1c1943f09b558bcfd6ce2da06b98e4dec110d2ea
gucci-ninja <beautyspell5@gmail.com>
2018-06-12 02:10:52 -0400

    changed path to Home

--- yak-yik/src/app.js
+++ yak-yik/src/app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react' // Component is a submodule
 // var React = require('react') --- this is es5, we are using es6
 import ReactDOM from 'react-dom'
-import Zones from './components/Zones'
+// import Zones from './components/Zones'
+import Home from './components/layout/Home'
 
 // App is basically an html tag that we are gonna render in the dom
 class App extends Component {
     render(){
         return (
             <div>
-                Hello React! HOw are you doing? yoyo
-                <Zones />
+                YakYik!
+                <Home />
             </div>
         )
     }
