799ced61417a4b1fb42e54e083dd4ff0ed72b610
gucci-ninja <beautyspell5@gmail.com>
2018-07-11 01:53:05 -0400

    fixed zipcode array issue

--- yak-yik/src/components/pres/Zone.js
+++ yak-yik/src/components/pres/Zone.js
@@ -4,12 +4,14 @@ import styles from './styles'
 class Zone extends Component {   
     render(){
         const zoneStyle = styles.zone
+        // grab first zip
+        const zipCode = this.props.zone.zipCodes[0]
         return (
         <div style={zoneStyle.container}>
             <h2 style={zoneStyle.header}>
                 <a style={zoneStyle.title} href="#">{this.props.zone.name}</a>
             </h2>
-            <span className="detail">{this.props.zone.zipCode}</span><br />
+            <span className="detail">{zipCode}</span><br />
             <span className="detail">{this.props.zone.numComments} comments </span>
         </div>
         )
